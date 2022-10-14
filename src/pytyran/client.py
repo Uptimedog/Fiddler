c9fde08acbf3101a53ba6300ee0286a57720ac89
Clivern <hello@clivern.com>
2022-10-14 13:43:48 +0200

    chore: support API Key

--- src/pytyran/client.py
+++ src/pytyran/client.py
@@ -25,22 +25,31 @@ import requests
 
 class Client:
 
-    def __init__(self, base_url):
-        self.base_url = base_url
+    def __init__(self, base_url, api_key):
+        self._base_url = base_url
+        self._api_key = api_key
 
     def create_document(self, content, metadata):
-        url = f"{self.base_url}/api/v1/document"
+        url = f"{self._base_url}/api/v1/document"
 
-        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-API-Key": self._api_key,
+        }
 
         data = {"content": content, "metadata": metadata}
 
         return requests.post(url, json=data, headers=headers)
 
     def search_documents(self, text, metadata, limit):
-        url = f"{self.base_url}/api/v1/document/search"
+        url = f"{self._base_url}/api/v1/document/search"
 
-        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "X-API-Key": self._api_key,
+        }
 
         data = {"text": text, "limit": limit, "metadata": metadata}
 
